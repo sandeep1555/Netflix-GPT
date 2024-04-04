6cceea325ecb6f7a30b224f913d6e2abbed51960
Sandeep Dasari <sandeepdasari@Sandeeps-MacBook-Pro.local>
2024-04-04 06:17:33 +0530

    cra and setup tailwind

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl  text-red-700">
+      
+    Hi sandeep,create netflix-gpt without any deviation
+    </div>
+  );
+}
+
+export default App;
